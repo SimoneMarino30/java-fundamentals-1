fdd6347a6e560473787d8c6f2e1ff3cc75030771
Simone Marino <simo.marino@outlook.it>
2023-05-31 15:41:38 +0200

    Train Ticket ok

--- src/org/lerssons/java/FizzBuzz.java
+++ src/org/lerssons/java/FizzBuzz.java
@@ -3,5 +3,6 @@ package org.lerssons.java;
 public class FizzBuzz {
     public static void main(String[] args) {
         System.out.println("FizzBuzz works");
+
     }
 }
--- src/org/lerssons/java/TrainTicketPrice.java
+++ src/org/lerssons/java/TrainTicketPrice.java
@@ -1,7 +1,53 @@
 package org.lerssons.java;
 
+import java.util.Scanner;
+
 public class TrainTicketPrice {
     public static void main(String[] args) {
-        System.out.println("TrainTicketPrice works");
+
+        // Variables
+        int Km;
+        int Age;
+        double PricePerKm = 0.21;
+        double Discount;
+        double ReducedTicket;
+
+        // Discount's Variables
+        double Under18 = 20.00;
+        double Over65 = 40.00;
+
+        // Percentage Variable
+        double Per100 = 100.00;
+
+        // Scanner
+        Scanner Ask = new Scanner(System.in);
+
+        // Km request
+        System.out.println("How many kilometers do you wanna do? ");
+        Km = Ask.nextInt();
+
+        // Age request
+        System.out.println("How old are you? ");
+        Age = Ask.nextInt();
+
+        System.out.println("You wanna do " + Km + " Kilometers and you're " + Age + " years old");
+
+        double FullPrice = Km * PricePerKm;
+        // conditional percentage
+        if(Age < 18) {
+            Discount = ((FullPrice * Under18) / Per100);
+            System.out.println("Your discount will be: " + Discount + "€");
+            ReducedTicket = (FullPrice - Discount);
+            System.out.println("Your ticket will cost: " + ReducedTicket + "€");
+        }
+        else if(Age >= 65) {
+            Discount = ((FullPrice * Over65) / Per100);
+            System.out.println("Your discount will be: " + Discount + "€");
+            ReducedTicket = (FullPrice - Discount);
+            System.out.println("Your ticket will cost: " + ReducedTicket + "€");
+        }
+        else {
+            System.out.println("Your ticket will cost: " + FullPrice + "€");
+        }
     }
 }
