b25b50cf2feeae7405260cd1177c724f9e311dfd
Simone Marino <simo.marino@outlook.it>
2023-05-31 16:28:32 +0200

    FizzBuzz ok

--- src/org/lerssons/java/FizzBuzz.java
+++ src/org/lerssons/java/FizzBuzz.java
@@ -1,8 +1,25 @@
 package org.lerssons.java;
 
+// Import
+// import java.util.Scanner;
+
 public class FizzBuzz {
     public static void main(String[] args) {
-        System.out.println("FizzBuzz works");
 
+        // Variables
+        int Fizz = 3;
+        int Buzz = 5;
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % Fizz == 0 && i % Buzz == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % Fizz == 0) {
+                System.out.println("Fizz");
+            } else if (i % Buzz == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 }
