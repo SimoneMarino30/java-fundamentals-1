ff02f8d2816710ac43aa230fdc830dd946677070
Simone Marino <simo.marino@outlook.it>
2023-05-31 14:07:26 +0200

    First Commit

---
+++ src/org/lerssons/java/FizzBuzz.java
@@ -0,0 +1,7 @@
+package org.lerssons.java;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        System.out.println("FizzBuzz works");
+    }
+}
---
+++ src/org/lerssons/java/TrainTicketPrice.java
@@ -0,0 +1,7 @@
+package org.lerssons.java;
+
+public class TrainTicketPrice {
+    public static void main(String[] args) {
+        System.out.println("TrainTicketPrice works");
+    }
+}
