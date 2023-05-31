0d13f3543cafe25c27a25aee26865e737044fcbb
Simone Marino <simo.marino@outlook.it>
2023-05-31 15:47:35 +0200

    Train Ticket improved with best practises

--- src/org/lerssons/java/TrainTicketPrice.java
+++ src/org/lerssons/java/TrainTicketPrice.java
@@ -6,45 +6,45 @@ public class TrainTicketPrice {
     public static void main(String[] args) {
 
         // Variables
-        int Km;
-        int Age;
-        double PricePerKm = 0.21;
-        double Discount;
-        double ReducedTicket;
+        int km;
+        int age;
+        double pricePerKm = 0.21;
+        double discount;
+        double reducedTicket;
 
         // Discount's Variables
-        double Under18 = 20.00;
-        double Over65 = 40.00;
+        double under18 = 20.00;
+        double over65 = 40.00;
 
         // Percentage Variable
-        double Per100 = 100.00;
+        double per100 = 100.00;
 
         // Scanner
-        Scanner Ask = new Scanner(System.in);
+        Scanner ask = new Scanner(System.in);
 
         // Km request
         System.out.println("How many kilometers do you wanna do? ");
-        Km = Ask.nextInt();
+        km = ask.nextInt();
 
         // Age request
         System.out.println("How old are you? ");
-        Age = Ask.nextInt();
+        age = ask.nextInt();
 
-        System.out.println("You wanna do " + Km + " Kilometers and you're " + Age + " years old");
+        System.out.println("You wanna do " + km + " Kilometers and you're " + age + " years old");
 
-        double FullPrice = Km * PricePerKm;
+        double FullPrice = km * pricePerKm;
         // conditional percentage
-        if(Age < 18) {
-            Discount = ((FullPrice * Under18) / Per100);
-            System.out.println("Your discount will be: " + Discount + "€");
-            ReducedTicket = (FullPrice - Discount);
-            System.out.println("Your ticket will cost: " + ReducedTicket + "€");
+        if(age < 18) {
+            discount = ((FullPrice * under18) / per100);
+            System.out.println("Your discount will be: " + discount + "€");
+            reducedTicket = (FullPrice - discount);
+            System.out.println("Your ticket will cost: " + reducedTicket + "€");
         }
-        else if(Age >= 65) {
-            Discount = ((FullPrice * Over65) / Per100);
-            System.out.println("Your discount will be: " + Discount + "€");
-            ReducedTicket = (FullPrice - Discount);
-            System.out.println("Your ticket will cost: " + ReducedTicket + "€");
+        else if(age >= 65) {
+            discount = ((FullPrice * over65) / per100);
+            System.out.println("Your discount will be: " + discount + "€");
+            reducedTicket = (FullPrice - discount);
+            System.out.println("Your ticket will cost: " + reducedTicket + "€");
         }
         else {
             System.out.println("Your ticket will cost: " + FullPrice + "€");
